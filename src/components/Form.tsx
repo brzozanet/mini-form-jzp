72adb9d21986fcd1e95f7e92cb2d8d4e6a2253fb
Paweł Brzozowski <pawel@brzoza.net>
2026-08-29 18:54:29 +0200

    style: format CSS and JSX for consistency

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,33 +1,29 @@
 import "./Form.css";
 
 export function Form({ onAddPerson }) {
-    return (
-        <form autoComplete="off">
-            <label htmlFor="name">Imię</label>
-            <input id="name" />
+  return (
+    <form autoComplete="off">
+      <label htmlFor="name">Imię</label>
+      <input id="name" />
 
-            <label htmlFor="age">Wiek</label>
-            <input id="age" type="number" />
+      <label htmlFor="age">Wiek</label>
+      <input id="age" type="number" />
 
-            <label htmlFor="tel">Telefon</label>
-            <input id="tel" type="tel" />
+      <label htmlFor="tel">Telefon</label>
+      <input id="tel" type="tel" />
 
-            <label htmlFor="email">E-mail</label>
-            <input id="email" type="email" />
+      <label htmlFor="email">E-mail</label>
+      <input id="email" type="email" />
 
-            <label htmlFor="isInvoiceRequired">
-                <input
-                    id="isInvoiceRequired"
-                    type="checkbox"
-                    placeholder="Podaj NIP"
-                />
-                Faktura VAT
-            </label>
-            <input id="nip" />
+      <label htmlFor="isInvoiceRequired">
+        <input id="isInvoiceRequired" type="checkbox" placeholder="Podaj NIP" />
+        Faktura VAT
+      </label>
+      <input id="nip" />
 
-            <div className="footer">
-                <button>Dodaj</button>
-            </div>
-        </form>
-    );
+      <div className="footer">
+        <button>Dodaj</button>
+      </div>
+    </form>
+  );
 }
