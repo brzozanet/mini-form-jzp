52ff3a4340ed3b99f2e056b04e4251a889ba53ae
Paweł Brzozowski <pawel@brzoza.net>
2026-09-02 08:36:54 +0200

    Implement controlled inputs in Form component and add form submission handler

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,26 +1,38 @@
+import { useState } from "react";
 import type { FormProps } from "../types/types";
 import "./Form.css";
 
 export function Form({ onAddPerson }: FormProps) {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [tel, setTel] = useState("");
+  const [email, setEmail] = useState("");
+  const [isInvoiceRequired, setIsInvoiceRequired] = useState(false);
+  const [nip, setNip] = useState("");
+
+  const handleFormSubmit = (event) => {
+    console.log(event);
+  };
+
   return (
-    <form autoComplete="off">
+    <form autoComplete="off" onSubmit={handleFormSubmit}>
       <label htmlFor="name">Imię</label>
-      <input id="name" />
+      <input id="name" value={name} />
 
       <label htmlFor="age">Wiek</label>
-      <input id="age" type="number" />
+      <input id="age" type="number" value={age} />
 
       <label htmlFor="tel">Telefon</label>
-      <input id="tel" type="tel" />
+      <input id="tel" type="tel" value={tel} />
 
       <label htmlFor="email">E-mail</label>
-      <input id="email" type="email" />
+      <input id="email" type="email" value={email} />
 
       <label htmlFor="isInvoiceRequired">
         <input id="isInvoiceRequired" type="checkbox" placeholder="Podaj NIP" />
         Faktura VAT
       </label>
-      <input id="nip" />
+      <input id="nip" value={nip} />
 
       <div className="footer">
         <button>Dodaj</button>
@@ -28,3 +40,5 @@ export function Form({ onAddPerson }: FormProps) {
     </form>
   );
 }
+
+// (parameter) e: React.SubmitEvent<HTMLFormElement>
