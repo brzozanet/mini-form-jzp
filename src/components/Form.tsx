33d296d61ee40c79d614e19aa2fb2f87960c8c9d
Paweł Brzozowski <pawel@brzoza.net>
2026-09-13 22:24:59 +0200

    Refactor: reorganize state management in Form component and add missing input fields

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import "./Form.css";
 
 export function Form({ onAddPerson }) {
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+  const [age, setAge] = useState("");
+  const [tel, setTel] = useState("");
+  const [email, setEmail] = useState("");
+  const [isInvoiceRequired, setIsInvoiceRequired] = useState(false);
+  const [nip, setNip] = useState("");
+
   return (
     <form autoComplete="off">
       <label htmlFor="name">Imię</label>
       <input id="name" />
+
+      <label htmlFor="surname">Nazwisko</label>
+      <input id="surname" />
 
       <label htmlFor="age">Wiek</label>
       <input id="age" type="number" />
